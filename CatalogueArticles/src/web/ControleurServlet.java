5f9a37e33fb082799dcad1bcae5bfcad9ab4b823
itahir <idrisstahir@gmail.com>
2018-02-18 21:08:53 +0100

    Projet catalogue - ajout exemples d'écrans

--- CatalogueArticles/src/web/ControleurServlet.java
+++ CatalogueArticles/src/web/ControleurServlet.java
@@ -16,6 +16,7 @@ import metier.entites.Article;
 
 public class ControleurServlet extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
 	private IArticleDao metier;
 	
 	@Override
